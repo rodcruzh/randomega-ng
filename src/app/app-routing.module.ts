d8e1400370bc88973e04e81db07195bfd8299764
Rodrigo Cruz H <rodrigo.cruz.hz@gmail.com>
2018-08-08 14:58:44 -0500

    Routing to Category Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { BlogCategoryComponent } from './blog-category/blog-category.component';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-    { path:'', redirectTo: '/app', pathMatch: 'full' },
-    { path: 'app', component: AppComponent }
+    { path: '', redirectTo: '/category', pathMatch: 'full' },
+    { path: 'app', component: AppComponent },
+    { path: 'category', component: BlogCategoryComponent },
 ]
 
 @NgModule({
