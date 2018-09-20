ca2f50c60431d586d9960505936dc5769683fb52
Rodrigo Cruz H <rodrigo.cruz.hz@gmail.com>
2018-09-20 07:59:28 -0500

    Web basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
-import { BlogCategoryComponent } from './blog-category/blog-category.component';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/category', pathMatch: 'full' },
+    { path: '', redirectTo: '/app', pathMatch: 'full' },
     { path: 'app', component: AppComponent },
-    { path: 'category', component: BlogCategoryComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogCategoryComponent } from './blog-category/blog-category.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogCategoryComponent
+    BlogCategoryComponent,
+    HeaderComponent,
+    MenuComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
